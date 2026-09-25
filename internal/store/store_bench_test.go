8834f08d55db27502390c3209524f44dc0abf78c
precious1joe <162345921+precious1joe@users.noreply.github.com>
2026-09-25 13:51:33 +0100

    Fix PR #1001: update internal/store/store_bench_test.go

--- internal/store/store_bench_test.go
+++ internal/store/store_bench_test.go
@@ -15,6 +15,16 @@ func generateDummyEvents(startLedger uint32, count int) []any {
 	return res
 }
 
+func generateDummyPayloads(startLedger uint32, count int) []any {
+	res := make([]any, count)
+	for i := 0; i < count; i++ {
+		res[i] = map[string]any{
+			"ledger": startLedger + uint32(i),
+		}
+	}
+	return res
+}
+
 func buildQuery(f EventFilter) (string, []any, error) {
 	query := "SELECT * FROM events WHERE 1=1"
 	var args []any
@@ -72,7 +82,7 @@ func BenchmarkUpsertBatchMemory_1000(b *testing.B) {
 }
 
 func benchmarkUpsertPayload(b *testing.B, size int) {
-	events := generateDummyEvents(100000, size)
+	events := generateDummyPayloads(100000, size)
 	b.ResetTimer()
 	b.ReportAllocs()
 	for i := 0; i < b.N; i++ {
